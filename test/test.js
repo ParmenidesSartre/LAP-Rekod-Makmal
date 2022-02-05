b4a9836af18374b091c808c19eaa1a5ac0b46b72
Faizal Azman <faizalazman88@gmail.com>
2022-02-05 14:08:26 +0800

    add test
    Add test for  '/api/rekod-strelise/:id' route

--- test/test.js
+++ test/test.js
@@ -6,10 +6,24 @@ const app = require('../server')
 describe('/api/rekod-strelise routes', () => {
   let fakeDb = require('../models/records.model')
   describe('GET /api/rekod-strelise', () => {
+
     it('should return all records', () => {
       return request(app).get('/api/rekod-strelise').expect(200).then((res) => {
         expect(res.body.data).to.be.an('array')
       })
     })
+
+    it('should return strelise record by id', () => {
+        return request(app).get('/api/rekod-strelise/1').expect(200).then((res) => {
+            expect(res.body.data).to.be.an('object')
+            expect(res.body.data.id).to.equal(1)
+            expect(res.body.data).has.ownProperty('name')
+            expect(res.body.data).has.ownProperty('date')
+            expect(res.body.data).has.ownProperty('place')
+            expect(res.body.data).has.ownProperty('location')
+            expect(res.body.data.location).to.be.an('array')
+        })
+    })
+
   })
 })
